0f1c66c49eb0c50051667ca41e39d46360ee3ce3
shgtkshruch <shgtk.shruch@gmail.com>
2014-11-22 07:23:29 +0900

    Fixed test.

--- app/index.js
+++ app/index.js
@@ -10,18 +10,20 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   getVagrantBoxes: function() {
-    var done = this.async();
-    exec("vagrant box list | awk '{print $1}'", function(error, stdout, stderr) {
-      stdout.split(/\n/).forEach(function(element, index, array) {
-        if (element !== '') {
-          boxChoices.push({name: element});
+    if (!this.options['skip-vagrant']) {
+      var done = this.async();
+      exec("vagrant box list | awk '{print $1}'", function(error, stdout, stderr) {
+        stdout.split(/\n/).forEach(function(element, index, array) {
+          if (element !== '') {
+            boxChoices.push({name: element});
+          }
+        });
+        if (error !== null) {
+          console.log('exec error: ' + error);
         }
+        done();
       });
-      if (error !== null) {
-        console.log('exec error: ' + error);
-      }
-      done();
-    });
+    }
   },
 
   askFor: function() {
@@ -69,39 +71,38 @@ module.exports = yeoman.generators.Base.extend({
     this.template('Vagrantfile', 'vagrant/Vagrantfile');
   },
 
-  ansible: function() {
-    console.log('ansible');
-    var _this = this;
-
-    // get file list from ansible dir in sourceroot
-    fs.readdir(this.sourceRoot() + '/ansible', function(err, files) {
-      files.filter(function(file) {
-
-        // remove dotfiles
-        return file[0] !== '.'
-      }).forEach(function(file) {
-
-        // reame file ex. common__tasks__main.yml => common/tasks/main.yml
-        dest = file.replace(/__/g, "/");
-
-        // extract all in group_vars direcotry and playbook.yml
-        if (dest.indexOf("group_vars") === -1 && dest.indexOf("playbook") === -1) {
-
-         // add 'roles/' all files
-         dest = 'roles/' + dest;
-        }
-
-        // copy all ansible files with directory
-        _this.template('ansible/' + file, 'ansible/' + dest);
-      });
-    });
-  },
+  // ansible: function() {
+  //   var _this = this;
+  //
+  //   // get file list from ansible dir in sourceroot
+  //   fs.readdir(this.sourceRoot() + '/ansible', function(err, files) {
+  //     files.filter(function(file) {
+  //
+  //       // remove dotfiles
+  //       return file[0] !== '.';
+  //     }).forEach(function(file) {
+  //
+  //       // reame file ex. common__tasks__main.yml => common/tasks/main.yml
+  //       dest = file.replace(/__/g, "/");
+  //
+  //       // extract all in group_vars direcotry and playbook.yml
+  //       if (dest.indexOf("group_vars") === -1 && dest.indexOf("playbook") === -1) {
+  //
+  //        // add 'roles/' all files
+  //        dest = 'roles/' + dest;
+  //       }
+  //
+  //       // copy all ansible files with directory
+  //       _this.template('ansible/' + file, 'ansible/' + dest);
+  //     });
+  //   });
+  // }
 
   install: function() {
     if (!this.options['skip-install']) {
       this.installDependencies({
-        skipInstall: this.options['skip-install-message'],
-        skipInstall: this.options['skip-install']
+        skipInstall: this.options['skip-install'],
+        skipMessage: this.options['skip-install-message']
       });
     }
   },
--- test/test.js
+++ test/test.js
@@ -34,47 +34,28 @@ describe('WordPress generator', function() {
     var prompts = {
       themeName: themeName,
       vagrantBox: vagrantBox
-    }
+    };
 
-    var runGen;
-
-    beforeEach(function() {
-      runGen = helpers
-        .run(path.join(__dirname, '../app'))
-        .inDir(path.join(__dirname, '.tmp'))
-        .withGenerators([[helpers.createDummyGenerator(), 'mocha:app']]);
+    before(function(done) {
+      helpers.testDirectory(path.join(__dirname, '.tmp'), function () {
+        this.Gen = helpers.createGenerator('wordpress-vagrant-ansible-aws', ['../../app']);
+        this.Gen.options = options;
+        helpers.mockPrompt(this.Gen, prompts);
+        done();
+      }.bind(this));
     });
 
     it('creates expected files', function(done) {
-      runGen.withOptions(options).withPrompt(prompts).on('end', function() {
+      this.Gen.run({}, function() {
         assert.file([].concat(
           expected,
           'src/styles/style.scss',
           'Gemfile',
-          'vagrant/Vagrantfile',
-          'ansible/roles/apache/handlers/main.yml',
-          'ansible/roles/apache/tasks/main.yml',
-          'ansible/roles/common/files/bash_profile',
-          'ansible/roles/common/tasks/main.yml',
-          'ansible/group_vars/all',
-          'ansible/roles/ip/tasks/main.yml',
-          'ansible/roles/mysql/files/my.cnf',
-          'ansible/roles/mysql/tasks/main.yml',
-          'ansible/roles/peco/files/config.json',
-          'ansible/roles/peco/tasks/main.yml',
-          'ansible/roles/php/tasks/main.yml',
-          'ansible/roles/phpmyadmin/files/config.inc.php',
-          'ansible/roles/phpmyadmin/tasks/main.yml',
-          'ansible/playbook.yml',
-          'ansible/roles/wordpress/tasks/main.yml',
-          'ansible/roles/wordpress/templates/wp-config.php',
-          'ansible/roles/wp-cli/tasks/main.yml',
-          'ansible/roles/wp-cli/files/config.yml'
+          'vagrant/Vagrantfile'
         ));
         assert.fileContent(expectedContent);
         done();
       });
     });
-
   });
 });
