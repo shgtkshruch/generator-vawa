0cac37372b700a82a463a50127e3fd6c5bd28ae4
shgtkshruch <shgtk.shruch@gmail.com>
2015-07-12 19:56:42 +0900

    Add template tag to index.php

--- app/templates/index.php
+++ app/templates/index.php
@@ -4,8 +4,12 @@
 	<meta charset="UTF-8">
 	<title></title>
   <meta name="viewport" content="width=device-width,initial-scale=1">
+  <?php wp_head(); ?>
 </head>
 <body>
 	Hello WordPress
+
+  <?php wp_footer(); ?>
+</head>
 </body>
 </html>
